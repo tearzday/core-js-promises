6c72a699da0b25d0d413fa5812127288a6037331
tearzday <biverdaniil@gmail.com>
2025-04-09 12:02:47 +0300

    feat: add getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -112,8 +112,15 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((body) => {
+    return body.map((item) => {
+      if (item.status === 'fulfilled') {
+        return item.value;
+      }
+      return null;
+    });
+  });
 }
 
 /**
